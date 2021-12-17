0f75b286fd2c978bfd0b475806381d70e3a079ab
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-12-16 21:36:00 -0800

    Extend scope resolution pass

--- jlox/com/craftinginterpreters/lox/Environment.java
+++ jlox/com/craftinginterpreters/lox/Environment.java
@@ -15,23 +15,6 @@ class Environment {
         this.enclosing = enclosing;
     }
 
-    Object get(Token name) {
-        if (values.containsKey(name.lexeme)) {
-            return values.get(name.lexeme);
-        }
-
-        if (enclosing != null) {
-            return enclosing.get(name);
-        }
-
-        throw new RuntimeError(name,
-            "Undefined variable '" + name.lexeme + "'.");
-    }
-
-    void define(String name, Object value) {
-        values.put(name, value);
-    }
-
     void assign(Token name, Object value) {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
@@ -45,5 +28,35 @@ class Environment {
 
         throw new RuntimeError(name,
             "Undefined variable '" + name.lexeme + "'.");
+    }
+
+    Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+
+        return environment;
+    }
+
+    void define(String name, Object value) {
+        values.put(name, value);
+    }
+
+    Object get(Token name) {
+        if (values.containsKey(name.lexeme)) {
+            return values.get(name.lexeme);
+        }
+
+        if (enclosing != null) {
+            return enclosing.get(name);
+        }
+
+        throw new RuntimeError(name,
+            "Undefined variable '" + name.lexeme + "'.");
+    }
+
+    Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
     }
 }
