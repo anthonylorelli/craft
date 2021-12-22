38d9a336683250245e5e6ae6f6b84f8265423e19
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-12-21 17:20:53 -0800

    Implement set expressions

--- jlox/com/craftinginterpreters/lox/Parser.java
+++ jlox/com/craftinginterpreters/lox/Parser.java
@@ -205,6 +205,9 @@ class Parser {
             if (expr instanceof Expr.Variable) {
                 Token name = ((Expr.Variable)expr).name;
                 return new Expr.Assign(name, value);
+            } else if (expr instanceof Expr.Get) {
+                Expr.Get get = (Expr.Get)expr;
+                return new Expr.Set(get.object, get.name, value);
             }
 
             error(equals, "Invalid assignment target.");
@@ -334,7 +337,7 @@ class Parser {
                 expr = finishCall(expr);
             } else if (match(DOT)) {
                 Token name = consume(IDENTIFIER,
-                    "Expect property name after '.'.")
+                    "Expect property name after '.'.");
                 expr = new Expr.Get(expr, name);
             } else {
                 break;
